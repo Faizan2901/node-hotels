f0e06f64c54d86956d4aabde614660da1c3064f9
Faizan2901 <mo.faizansopariwala2901@gmail.com>
2024-07-03 22:51:12 +0530

    add authentication in hotel project

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
 
 const workTypes = ['chef', 'waiter', 'manager'];
 
@@ -30,8 +31,58 @@ const personSchema = mongoose.Schema({
     salary: {
         type: Number,
         required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
     }
 });
+
+personSchema.pre('save', async function (next) {
+    const person = this
+
+    // if (!person.isModified('password')) return next()
+
+    try {
+        if (!person || !person.password) {
+            throw new Error('Person or password is not defined')
+        }
+
+        const salt = await bcrypt.genSalt(10)
+
+        if (!salt) {
+            throw new Error('Salt generation failed')
+        }
+
+        const hashedPassword = await bcrypt.hash(person.password, salt)
+
+        if (!hashedPassword) {
+            throw new Error('Password hashing failed')
+        }
+
+        person.password = hashedPassword
+        next()
+    } catch (err) {
+        console.error('Error hashing password:', err)
+        return next(err)
+    }
+
+
+
+})
+
+personSchema.methods.comparePassword = async function (candidatePassword) {
+    try {
+        const isMatch = await bcrypt.compare(candidatePassword, this.password)
+        return isMatch
+    } catch (err) {
+        throw err
+    }
+}
 
 const Person = mongoose.model('Person', personSchema);
 module.exports = Person;
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
-const Person = require('./models/person')
 const MenuItem = require('./models/menu')
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 require('dotenv').config();
 const PORT = process.env.PORT || 3000
+const passport = require('./auth')
+
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to : ${req.originalUrl}`);
+    next();
+}
 
 const personRoutes = require('./routes/personRoutes')
 const menuItemsRoutes = require('./routes/menuItemsRoutes')
 
+app.use(logRequest)
+app.use(passport.initialize())
+const localAuthMiddleware = passport.authenticate('local', { session: false })
+
 app.use('/person', personRoutes)
-app.use('/menu', menuItemsRoutes)
+app.use('/menu', localAuthMiddleware, menuItemsRoutes)
 
 app.get('/', (req, res) => {
     res.send(`
